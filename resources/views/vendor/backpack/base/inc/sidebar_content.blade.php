818a96991eb8254638589166f0d6a355713e9bb7
danihadi <detal31392@gmail.com>
2021-11-17 12:53:58 +0700

    fixed capitalize function name

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,5 +1,5 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-@foreach ((new App\Helpers\sidebar())->generate() as $key => $menu)
+@foreach ((new App\Helpers\Sidebar())->generate() as $key => $menu)
     @if(in_array(backpack_auth()->user()->role->name, $menu['roles']))
     <li class="nav-item"><a class="nav-link" href="{{  $menu['url'] }}"><i class="la {{$menu['icon']}} nav-icon"></i> {{$menu['name']}}</a></li>
     @endif
