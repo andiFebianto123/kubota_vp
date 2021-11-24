83c0b0326993ffa510669ff57b4c7d09b95b5f85
andiFebianto123 <andifebianto123@gmail.com>
2021-11-24 15:38:57 +0700

    Jangan pake tombol buat email OTP nya fix

--- resources/views/emails/sample-mail.blade.php
+++ resources/views/emails/sample-mail.blade.php
@@ -12,7 +12,7 @@
     Reset Password
 @endcomponent
 @elseif($details['type'] == 'otp')
-@component('mail::button', ['url' => $details['otp_url']])
+@component('mail::button', ['url' => $details['otp_url'], 'type' => 'OTP'])
     {{ $details['otp_code'] }}
 @endcomponent
 @endif
--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -7,7 +7,11 @@
 <table border="0" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
 <td>
+@if (isset($type))
+<span>{{ $slot }}</span>
+@else
 <a href="{{ $url }}" class="button button-{{ $color ?? 'primary' }}" target="_blank" rel="noopener">{{ $slot }}</a>
+@endif
 </td>
 </tr>
 </table>
