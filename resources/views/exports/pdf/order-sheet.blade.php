a96f01f20d3dcd0a6c12ff2e578f4d6e1b4cd369
danihadi <detal31392@gmail.com>
2021-12-21 21:58:09 +0700

    pdf order sheet

--- resources/views/exports/pdf/order-sheet.blade.php
+++ resources/views/exports/pdf/order-sheet.blade.php
@@ -8,91 +8,220 @@
     <title>PDF</title>
 </head>
 <style>
-    table{
+    .table-items {
+        font-size: 10px;
+        width: 100%;
+        margin-top: 10px;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .table-items td {
+        padding: 2px;
+        background-color: #ffffff;
+        border-right: 1px solid #000000;
+        border-bottom: 0px;
+    }
+
+    .table-items th {
+        border-right: 1px solid #000000;
+        border-top: 1px solid #000000;
+        border-bottom: 1px solid #000000;
+    }
+
+    .unit-sheet {
+        border: 1px solid #000000;
+    }
+
+    .header-section {
+        margin: 10px 10px 5px 10px;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 10px;
+    }
+
+    .header-section .title {
+        font-weight: bold;
+        font-size: 14px;
+    }
+
+    .email-and-issued {
+        font-size: 10px;
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 10px 10px 15px 10px;
+    }
+
+    .title-table-right {
+        font-weight: bold;
+        font-size: 14px;
+    }
+
+    .box-check {
+        border: 1px solid #000000;
+        width: 20px;
+        height: 10px;
+        color: #ffffff;
+    }
+
+    .total-price {
         font-size: 12px;
+        padding: 4px;
+        text-align: center;
+        border-bottom: 1px solid #000000;
+    }
+
+    .doc-req {
+        padding: 4px 10px 4px 10px;
+        width: 80%;
+    }
+
+    footer {
+        position: fixed;
+        bottom: -60px;
+        left: 0px;
+        right: 0px;
+        height: 50px;
+        color: #000000;
+        font-size: 12px;
+        /** Extra personal styles **/
+        /* background-color: #03a9f4;
+        color: white;
+        text-align: center;
+        line-height: 35px; */
+    }
+    .pagenum:before {
+        content: counter(page);
     }
 </style>
+
 <body>
-    <span>Order Sheet PT KUBOTA INDONESIA</span>
-    <div>
-        <table class="table">
-            <tr>
-                <td>PO Number</td>
-                <td>: {{$po->po_num}}</td>
-            </tr>
-            <tr>
-                <td>Vendor</td>
-                <td>: {{$po->vend_num}}</td>
-            </tr>
-            <tr>
-                <td>PO Date</td>
-                <td>: {{date('Y-m-d', strtotime($po->po_date))}}</td>
-            </tr>
-            <tr>
-                <td>Email Sent</td>
-                <td>: {{($po->email_flag) ? "✓":"-"}}</td>
-            </tr>
-        </table>
-    </div>
-
-    <div>
-        <h5>List PO Line</h5>
-        <table class="table table-striped mb-0 table-responsive" >
-            <thead  style="border-top: 1px solid #000000 ; border-bottom: 1px solid #000000 ;">
+    <div class="unit-sheet">
+        <div class="header-section">
+            <table style="width:100%;">
                 <tr>
-                    <th>PO Number</th>
-                    <th>Status</th>
-                    <th>Item</th>
-                    <th>Vendor Name</th>
-                    <th>Description</th>
-                    <th>Qty</th>
-                    <th>UM</th>
-                    <th>Due Date</th>
-                    <th>Tax (%)</th>
-                    <th>Unit Price</th>
-                    <th>Total Price</th>
-                    <th>Status Accept</th>
-                    <th>Read By</th>
-                    <th>Read At</th>
-                    @if(backpack_auth()->user()->hasRole('Admin PTKI'))
-                        <th>Created At</th>
-                    @endif
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($po_lines as $key => $po_line)
-                <tr>
-                    <td class="text-nowrap">{{$po_line->po_num}}-{{$po_line->po_line}}</td>
-                    <td>
-                        <span class="{{$arr_po_line_status[$po_line->status]['color']}}">
-                            {{$arr_po_line_status[$po_line->status]['text']}}
-                        </span>
+                    <td valign="top">
+                        <label class="title">ORDER SHEET {{$po->po_num}} Rev 00</label>
+                        <p>{{$po->vend_num}} - {{$po->vendor->vend_name}}</p>
+                        <p>Attn : <br> {{$po->vendor->vend_name}}<br> {{$po->vendor->vend_addr}}</p>
                     </td>
-                    <td>{{$po_line->item}}</td>
-                    <td>{{$po_line->vendor_name}}</td>
-                    <td>{{$po_line->description}}</td>
-                    <td>{!! $po_line->order_qty !!}</td>
-                    <td>{{$po_line->u_m}}</td>
-                    <td>{!! $po_line->due_date !!}</td>
-                    <td>{{$po_line->tax}}</td>
-                    <td class="text-nowrap">{!! $po_line->unit_price !!}</td>
-                    <td class="text-nowrap">{!! $po_line->total_price !!}</td>
-                    <td>{!! $po_line->flag_accept !!}</td>
-                    <td>{{$po_line->read_by_user}}</td>
-                    <td>{{$po_line->read_at}}</td>
-                    @if(backpack_auth()->user()->hasRole('Admin PTKI'))
-                        <td>{{$po_line->created_at}}</td>
-                    @endif
-                   
+                    <td valign="top" align="right">
+                        <img src="{{ public_path('/img/logokubotaforearth.png')}}" width="250px"><br>
+                        <div style="text-align: left; width:280px; float:right">
+                            <span class="title-table-right">PT. KUBOTA INDONESIA</span> <br>
+                            Taman Industri Bukit Semarang Baru (BSB) Blok D.1 Kav.8 <br> Kel.Jatibarang - Kec. Mijen – Kota Semarang
+                            <table>
+                                <tr>
+                                    <td>Telp</td>
+                                    <td>: 024-747289, 7473257</td>
+                                </tr>
+                                <tr>
+                                    <td>Fax</td>
+                                    <td>: 024-7474266, 7472865</td>
+                                </tr>
+                                <tr>
+                                    <td>Email</td>
+                                    <td>: ptki_g.layanan@kubota.com </td>
+                                </tr>
+                                <tr>
+                                    <td>Website</td>
+                                    <td>: www.ptkubota.co.id</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </td>
                 </tr>
-                @endforeach
+            </table>
+        </div>
+        <div style="clear: both;"></div>
+        <div class="email-and-issued">
+            <div style="float: left;">
+                Email : {{$po->vendor->vend_email}}
+            </div>
+            <div style="float: right;">
+                <b>Issued Date : {{date("Y-m-d", strtotime($po->po_date))}}</b>
+            </div>
+        </div>
+        <div style="clear: both;"></div>
+        <div>
+            <table class="table-items" border="0" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>ORDER NO</th>
+                        <th>REV</th>
+                        <th>ITEM NO</th>
+                        <th>DESCRIPTION</th>
+                        <th>DUE DATE</th>
+                        <th>QTY ORDER</th>
+                        <th>UNIT PRICE ({{$po->vendor->currency}})</th>
+                        <th>TOTAL AMOUNT ({{$po->vendor->currency}})</th>
+                        <th>PROD DATE (Ref)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php
+                    $total = 0;
+                    @endphp
+                    @foreach ($po_lines as $key => $po_line)
+                    @php
+                    $total += $po_line->order_qty*$po_line->unit_price;
+                    @endphp
+                    <tr>
+                        <td align="center">{{$key+1}}</td>
+                        <td align="center" class="text-nowrap">{{$po_line->po_num}}-{{$po_line->po_line}}</td>
+                        <td align="center">00</td>
+                        <td>{{$po_line->item}}</td>
+                        <td>{{$po_line->description}}</td>
+                        <td align="center">{!! date("Y-m-d", strtotime($po_line->due_date)) !!}</td>
+                        <td align="right">{{$po_line->order_qty}}</td>
+                        <td align="right" class="text-nowrap"> {{number_format($po_line->unit_price,0,',','.')}}</td>
+                        <td align="right" class="text-nowrap">{{number_format($po_line->order_qty*$po_line->unit_price,0,',','.')}}</td>
+                        <td align="center">{!! date("Y-m-d", strtotime($po_line->production_date)) !!}</td>
+                    </tr>
+                    @endforeach
+                    <tr>
+                        <td colspan="5" valign="bottom" style="border-top: 1px solid #000000; padding:4px;">
+                            <b>*) Computer direct to PDF Format, No Signature is required</b>
+                        </td>
+                        <td colspan="5" style="border-top: 1px solid #000000;">
+                            <div class="total-price">TOTAL <b>{{$po->vendor->currency}} {{number_format($total,0,',','.')}}</b></div>
+                            <div class="doc-req">
+                                <p>NOTE : Document's Requirements :</p>
+                                <div>
+                                    <div class="box-check" style="float: left;"></div>
+                                    <div style="float: left; margin-left:6px;">Material Mill Sheet</div>
+                                </div>
+                                <div style="clear:both"></div>
+                                <div>
+                                    <div class="box-check" style="float: left;"></div>
+                                    <div style="float: left; margin-left:6px;">Material Safety Data Sheet</div>
+                                </div>
+                                <div style="clear:both"></div>
+                                <div>
+                                    <div class="box-check" style="float: left;"></div>
+                                    <div style="float: left; margin-left:6px;">Result Of Inspection (Certificate)</div>
+                                </div>
+                                <div style="clear:both"></div>
+                                <div>
+                                    <div class="box-check" style="float: left;"></div>
+                                    <div style="float: left; margin-left:6px;">Product Safety Information Sheet</div>
+                                </div>
+                                <div style="clear:both"></div>
+                                <div>
+                                    <div class="box-check" style="float: left;"></div>
+                                    <div style="float: left; margin-left:6px;">Instruction Operator Manual</div>
+                                </div>
+                                <div style="clear:both"></div>
 
-            </tbody>
-        </table>
+                            </div>
 
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-
-    
+    <footer>
+        Page <span class="pagenum"></span>
+    </footer>
 </body>
 
 </html>
