9e2cee1a8df5bb45b2352d9cc00ca9999e4561b8
andiFebianto123 <andifebianto123@gmail.com>
2022-02-18 10:54:23 +0700

    history mo per item fix

--- app/Http/Controllers/Admin/HistoriMoSummaryPerItemCrudController.php
+++ app/Http/Controllers/Admin/HistoriMoSummaryPerItemCrudController.php
@@ -66,7 +66,7 @@ class HistoriMoSummaryPerItemCrudController extends CrudController
             $this->crud->addClause('where', 'vend_num', '=', backpack_auth()->user()->vendor->vend_num);
         }
         $this->crud->groupBy('material_outhouse.matl_item');
-        $this->crud->query->havingRaw("(`pl`.`status` = 'C' or `pl`.`status` = 'F') or (`pl`.`status` = 'O' AND remaining_qty > 0)");
+        $this->crud->query->havingRaw("(`pl`.`status` = 'C' or `pl`.`status` = 'F') or (`pl`.`status` = 'O' AND remaining_qty = 0)");
 
         if(Constant::getRole() == 'Admin PTKI'){
             CRUD::column('vend_num')->label('Vend Num');
