3bfde617985218deedb2d659e1ce5c0b950c1af3
danihadi <detal31392@gmail.com>
2022-02-21 01:26:37 +0700

    fixed role selection

--- app/Http/Controllers/Admin/UserCrudController.php
+++ app/Http/Controllers/Admin/UserCrudController.php
@@ -24,6 +24,7 @@ use Illuminate\Support\Facades\Mail;
 use App\Mail\MailNewUser;
 use App\Models\Delivery;
 use App\Models\PurchaseOrderLine;
+use App\Models\Role;
 use Illuminate\Routing\Route;
 
 /**
@@ -78,7 +79,6 @@ class UserCrudController extends CrudController
         if(!Constant::checkPermission('Delete User')){
             $this->crud->removeButton('delete');
         }
-        $this->crud->addButtonFromView('top', 'upload_user', 'upload_user', 'end');
         CRUD::column('name');
         CRUD::column('username');
         CRUD::column('email');
@@ -98,7 +98,9 @@ class UserCrudController extends CrudController
                 $query->orWhere('roles.name', 'like', "%{$searchTerm}%");
             }
         ]);
-        if(!in_array(Constant::getRole(),['Admin PTKI'])){
+        if(in_array(Constant::getRole(),['Admin PTKI'])){
+            $this->crud->addButtonFromView('top', 'upload_user', 'upload_user', 'end');
+        }else{
             $this->crud->addClause('where', 'vendor_id', '=', backpack_auth()->user()->vendor->id);
         }
 
@@ -125,6 +127,10 @@ class UserCrudController extends CrudController
      */
     protected function setupCreateOperation()
     {
+        $allow_null_venodor = false;
+        if(in_array(Constant::getRole(),['Admin PTKI'])){
+            $allow_null_venodor = true;
+        }
         CRUD::setValidation(UserRequest::class);
        
         // $this->crud->addField([
@@ -136,21 +142,26 @@ class UserCrudController extends CrudController
         //     'model'     => "App\Models\Role",
         // ]);
 
-
-        $this->crud->addField([
+        $this->crud->addField([   // select2_from_array
             'label' => 'Role',
             'name' => 'roles',
             'attribute' => 'name',
-            'multiple' => false,
             'pivot' => false,
+            'multiple' => false,
+            'type' => 'relationship.relationship_select_roles',
+            'placeholder' => 'Select an entry',
+            // 'model' => \App\Models\Role::class,
+            // 'type'        => 'select2_from_array',
+            'options'     =>  $this->optRoles(),
         ]);
+
 
         $this->crud->addField([   // select2_from_array
             'name'        => 'vendor_id',
             'label'       => "Vendor",
             'type'        => 'select2_from_array',
             'options'     => $this->optVendors(),
-            'allows_null' => true,
+            'allows_null' => $allow_null_venodor,
         ]);
         
         CRUD::field('name');
@@ -185,6 +196,21 @@ class UserCrudController extends CrudController
         }
 
         return $arr_vendor;
+    }
+
+    private function optRoles()
+    {
+        if(in_array(Constant::getRole(),['Admin PTKI'])){
+            $roles = Role::get();
+        }else{
+            $roles = Role::where('name', '!=', 'Admin PTKI')->get();
+        }
+        $arr_roles = [];
+        foreach ($roles as $key => $r) {
+            $arr_roles[$r->id] = $r->name;
+        }
+
+        return $arr_roles;
     }
 
     private function handlePermissionNonAdmin($vendor_id){
