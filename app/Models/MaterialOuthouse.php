c8fef73044945a71e588e7635dc28bb93cb9a3d8
danihadi <detal31392@gmail.com>
2022-05-11 10:15:14 +0700

    fixed sum lot qty

--- app/Models/MaterialOuthouse.php
+++ app/Models/MaterialOuthouse.php
@@ -21,10 +21,22 @@ class MaterialOuthouse extends Model
             $join->on('delivery.ds_num', '=', 'issued_material_outhouse.ds_num');
             $join->on('delivery.ds_line', '=', 'issued_material_outhouse.ds_line');
         })
-         ->where('delivery.po_num', $this->po_num)
-         ->where('delivery.po_line', $this->po_line)
-         ->where('matl_item', $this->matl_item)
-         ->sum('issue_qty');
+        ->where('delivery.po_num', $this->po_num)
+        ->where('delivery.po_line', $this->po_line)
+        ->where('matl_item', $this->matl_item)
+        ->sum('issue_qty');
+
+        return $qtyIssued;
+    }
+
+
+    public function getSumLotQty()
+    {
+         $qtyIssued = MaterialOuthouse::where('po_num', $this->po_num)
+        ->where('po_line', $this->po_line)
+        ->where('matl_item', $this->matl_item)
+        ->where('seq', $this->seq)
+        ->sum('lot_qty');
 
         return $qtyIssued;
     }
@@ -33,7 +45,7 @@ class MaterialOuthouse extends Model
     public function getRemainingQtyAttribute()
     {
         $qtyIssued = $this->getQtyIssuedAttribute();
-        $qty = $this->lot_qty - $qtyIssued;
+        $qty = $this->getSumLotQty() - $qtyIssued;
         
         return $qty;
     }
