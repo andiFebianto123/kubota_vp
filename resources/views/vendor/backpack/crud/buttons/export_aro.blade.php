ac28dffe14e2e7a2fa669990f8eac6655a0cd332
danihadi <detal31392@gmail.com>
2022-09-09 10:10:20 +0700

    remove redundant function

--- resources/views/vendor/backpack/crud/buttons/export_aro.blade.php
+++ resources/views/vendor/backpack/crud/buttons/export_aro.blade.php
@@ -1,10 +1,11 @@
 @if ($crud->hasAccess('export_aro_excel'))
-    <button id="btn-for-export-aro-excel" class="btn btn-sm btn-primary-vp" data-route="{{$crud->exportAroRoute}}" type="button" onclick="exportExcel(this)" ><i class="la la-file-excel"></i> Export A/R/O</button>
+    <button id="btn-for-export-aro-excel" class="btn btn-sm btn-primary-vp" data-route="{{$crud->exportAroRoute}}" type="button" onclick="exportExcelAro(this)" ><i class="la la-file-excel"></i> Export A/R/O</button>
 @endif
 
 @push('after_scripts')
 <script>
-    function exportExcel(button){
+    function exportExcelAro(button){
+
         var route = $(button).attr('data-route');
         var expFilename = "{{(isset($crud->exportAroFilename)) ? $crud->exportFilename : date('YmdHis').'.xlsx'}}"
 
